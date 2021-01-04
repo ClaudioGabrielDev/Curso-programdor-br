09c4f3c77a2b615aae0c88a71dd19e2bb69b413b
Claudio Gabriel <designgabriel15@gmail.com>
2021-01-04 20:32:37 -0300

    Manipulando o DOM - pt 1/3
    Iniciando Manipulação do DOM

--- js - intermediario/script.js
+++ js - intermediario/script.js
@@ -6,7 +6,7 @@ for( let ps of p){
     console.log(ps)
 }
 
-console.log(p) */
+console.log(p) 
 
 var tt = document.getElementById("title")
 
@@ -19,5 +19,23 @@ tt.style.fontFamily = "Roboto"
 
 tt.setAttribute("meuAtt", "Qualquer coisa")
 
-tt.className = "tagTitle"
+tt.className = "tagTitle" */
+
+        
+    // MANIPULANDO O DOM
+
+function novoh1(i) {
+    
+    h2 = document.getElementsByTagName("h2")[0]
+
+    h2.innerText = i.value;
+    
+    
+}
+
+function hideTittle(){
+    h1 = document.getElementsByTagName("h1")[0];
+
+    h1.style.display = "none"
+}
 
