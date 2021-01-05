8b356aedaa1d241f5060a9a458db58332d9f6a4e
Claudio Gabriel <designgabriel15@gmail.com>
2021-01-05 20:31:25 -0300

    Manipulando o DOM pt 02 && 03/03
    Finalizando Manipulação do DOM, conhecimento muito proveitoso, principalmente referente ao "this".

--- js - intermediario/script.js
+++ js - intermediario/script.js
@@ -19,10 +19,10 @@ tt.style.fontFamily = "Roboto"
 
 tt.setAttribute("meuAtt", "Qualquer coisa")
 
-tt.className = "tagTitle" */
+tt.className = "tagTitle" 
 
         
-    // MANIPULANDO O DOM
+    // MANIPULANDO O DOM PT 1/3
 
 function novoh1(i) {
     
@@ -37,5 +37,55 @@ function hideTittle(){
     h1 = document.getElementsByTagName("h1")[0];
 
     h1.style.display = "none"
+}*/
+
+    // MANIPULANDO O DOM PT 2/3
+
+function Over(elemento){
+    elemento.style.backgroundColor = "red";
+}
+
+function Out(elementoOut){
+    elementoOut.style.backgroundColor = "rgb(60, 60, 185)";
+
+}
+function hidenTop(){
+    let topo = document.getElementsByTagName('img')[0];
+    let textButton = document.getElementById('bt')
+
+    if (topo.style.display == "none"){
+        textButton.innerText = "Click"
+        topo.style.display = "block"
+        
+    } else {
+        textButton.innerText = "Mostrar"
+        topo.style.display = "none"
+
+
+    }
+}
+
+        // MANIPULANDO O DOM PT 3/3
+
+let bt2 = document.getElementById('bt2')
+
+bt2.addEventListener("click", mudarCor);
+
+function mudarCor(){
+    this.style.backgroundColor = 'red';
+
+    let topo = document.getElementsByTagName('img')[0];
+
+    if (topo.style.display == "none"){
+        this.innerText = "Mesma coisa"
+        this.style.backgroundColor = 'rgb(60, 60, 185)';
+        topo.style.display = "block"
+        
+    } else {
+        this.innerText = "Mostrar"
+        topo.style.display = "none"
+
+
+    }
 }
 
