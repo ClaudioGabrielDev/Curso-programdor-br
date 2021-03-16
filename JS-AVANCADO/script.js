0bae842405432b20c63be81970af4a08900903da
Claudio Gabriel <designgabriel15@gmail.com>
2021-03-15 22:26:16 -0300

    Pregresso-nas-aulas
    Callback, Promise, Async Await e Filter.

--- JS-AVANCADO/script.js
+++ JS-AVANCADO/script.js
@@ -1,25 +1,25 @@
 // Titles
 var h1 = document.querySelector("titles")
 
-h1.style.fontFamily = "Arial"
-h1.style.color = "red"
+//h1.style.fontFamily = "Arial"
+//h1.style.color = "red"
 
 // tarefas
 // arrow function
-function dobro(x){
+function dobro(x) {
     console.log(2 * x)
 }
 
 dobro(2)
 
-let multi = function (x){
+let multi = function (x) {
     console.log(x * x)
 }
 
 multi(3);
 
-let multi2 = function(x){
-    return x * x 
+let multi2 = function (x) {
+    return x * x
 }
 
 console.log(multi2(3));
@@ -30,18 +30,103 @@ console.log(multi3(6))
 
 // callback
 
-let usuarios = ["Bianca", "Junior", "Thiago"]
+let usuarios = ["Claudio", "Gabriel", "Thiagos"];
 
-function incluirUsuario(nome, callback){
-    setTimeout(()=> {
+
+function usuario(nome, callback) {
+
+    setTimeout(() => {
         usuarios.push(nome);
-        callback();
-    }, 1000
-    )
-}
+        callback()
+    }, 1000);
 
-function listarUsuarios(){
+};
+
+function listarUsuarios() {
     console.log(usuarios)
 }
 
-incluirUsuario("Gabriel", listarUsuarios)
+usuario("Junior", listarUsuarios);
+
+// Promisses
+
+let usuarios2 = ["Claudio", "Gabriel", "Thiago"];
+
+
+function usuario2(nome1, nome2) {
+
+    let promise = new Promise(function (resolve, reject) {
+
+        setTimeout(() => {
+            usuarios2.push(nome1, nome2);
+
+            let error = false;
+
+            if(!error){
+                resolve();
+            }else{
+                reject({msg: "Erro de Qualquer"});
+            }
+        }, 1000);
+
+    })
+
+    return promise;
+};
+
+function listarUsuarios2() {
+    console.log(usuarios2)
+}
+
+//async await
+async function executar(){
+    await usuario2("Debora", "Junior")
+    listarUsuarios2();
+}
+
+executar();
+
+//Filter
+
+function novoAluno(nome, idade){
+    return {nome, idade}
+};
+
+let alunos = [
+    novoAluno("Gabriel", 23),
+    novoAluno("Bianca", 21),
+    novoAluno("Euclides", 42),
+    novoAluno("Claudio", 46)
+];
+
+function mais30(aluno){
+    return aluno.idade > 30
+}
+
+function menos30(aluno){
+    return aluno.idade < 30
+}
+
+console.log(alunos.filter(mais30));
+console.log(alunos.filter(menos30));
+
+function novoFilme(nome, genero){
+    return {nome, genero}
+}
+
+let filmes = [
+    novoFilme("Homem de Ferro", "Ação"),
+    novoFilme("A procura da Felicidade", "Drama"),
+    novoFilme("007", "Ação"),
+    novoFilme("Qualquer", "Drama")
+]
+
+function generoDrama(filme){
+    return filme.genero == "Drama"
+}
+function generoAcao(filme){
+    return filme.genero == "Ação"
+}
+
+console.log(filmes.filter(generoDrama))
+console.log(filmes.filter(generoAcao))
